015cbc433eeaca1e6cba9286e8559736aaa5c0ae
thanhlemm <tan036075@gmail.com>
2022-07-25 09:24:26 +0700

    All

--- assets/javascript/show.js
+++ assets/javascript/show.js
@@ -1,30 +1,25 @@
 var slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n){
     showSlides(slideIndex += n);
 }
-
-
 function showSlides(n){
     var i;
-    var slides = document.getElementsByClassName("slide");
+    var slides = document.getElementsByClassName("mySlides");
     if(n > slides.length){ slideIndex = 1}
     if(n < 1){slideIndex = slides.length}
     for(i = 0; i < slides.length; i++)
     {
         slides[i].style.display = "none";
     }
-   
 }
-
 
 //Auto slide
 var slideIndex = 0;
 showSlides();
 function showSlides(){
     var i;
-    var slides = document.getElementsByClassName("slide");
+    var slides = document.getElementsByClassName("mySlides");
     for(i = 0; i < slides.length; i++)
     {
         slides[i].style.display = "none";
